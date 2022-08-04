e30588bbfd748cd60e3c9b8df0ed2614c9748a44
shuotwang <shuotongwang@gmail.com>
2022-08-03 20:49:37 -0700

    add export to package

--- lib/voce_widgets.dart
+++ lib/voce_widgets.dart
@@ -2,9 +2,4 @@ library voce_widgets;
 
 export 'src/voce_button.dart' show VoceButton;
 export 'src/voce_textfield.dart' show VoceTextField;
-
-// /// A Calculator.
-// class Calculator {
-//   /// Returns [value] plus 1.
-//   int addOne(int value) => value + 1;
-// }
+export 'src/voce_text_input_formatter.dart' show VoceTextInputFormatter;
